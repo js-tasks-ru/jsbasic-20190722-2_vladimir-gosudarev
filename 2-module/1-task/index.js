e4f9ec1869f8fa83a00c4c078e5bb895963222e6
superqw3rty <vgosudarevv@gmail.com>
2019-08-07 19:11:41 +0300

    Решены задачи 2-го модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,19 @@
  * @returns {Object}
  */
 function clone(obj) {
+    let newObj= {};
+
+    if (!!obj === false) {
+        console.log('null / undefined');
+        return obj;
+    }
+    for (let key in obj) {
+        newObj[key] = obj[key];  
+        if (typeof(obj[key]) === 'object') {
+            console.log('object');
+            newObj[key] = clone(obj[key]);
+        } 
+    }
+    return newObj;
 }
+
