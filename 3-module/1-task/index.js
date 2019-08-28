e6caeb3f058c44ca217bed533243d8e38d227c08
sup3rqwerty <vgosudarevv@gmail.com>
2019-08-29 01:19:04 +0300

    решены задачи 3-го модуля

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,11 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+    let result = [];
+    for (let men of data) {
+        if (men.age <= age)   {
+            result.push(men.name + ', ' + men.balance);
+        }
+    }
+    return result.join('\n');
 }
--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,12 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+    let res = [];
+    let arr = str.split(' ').join(',').split(',');
+    for (let item of arr) {
+        if (isFinite(item) && item !== '') {
+        res.push(item);
+        }
+    }
+    return {min: Math.min(...res), max: Math.max(...res)};
 }
