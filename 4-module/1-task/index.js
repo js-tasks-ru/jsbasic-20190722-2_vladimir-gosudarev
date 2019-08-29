3eb64ff2abc96d53cd4375c65039bfc326f1e715
sup3rqwerty <vgosudarevv@gmail.com>
2019-08-29 23:49:16 +0300

    выполнен 4-й модуль

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,14 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+    const ul = document.createElement('ul');
+    let listItems = '';
+    for (friend of friends) {
+        listItems += `<li>${friend.firstName} ${friend.lastName}</li> `;
+    }
+    ul.innerHTML = listItems;
+    console.log(ul);
+    return ul;
 }
+
+
